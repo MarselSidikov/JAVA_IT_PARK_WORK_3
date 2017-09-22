59b223416a10e363a31fa12646b29445f2e54a67
student21 <student21@itpark.ru>
2017-09-22 20:56:15 +0300

    add task2

--- Tasks/Task1/Program.java
+++ Tasks/Task1/Program.java
@@ -1,7 +1,13 @@
+// импорт класса Scanner - модуль, позволяющий
+// считывать данные с какого либо потока
 import java.util.Scanner;
 class Program {
 	public static void main(String[] args) {
+	    // создаем объект класса Scanner
+	    // объектная переменная scanner
+	    // объект настроен на стандартный поток ввода
 		Scanner scanner = new Scanner(System.in);
+		// считали два числа
 		int x = scanner.nextInt();
 		int y = scanner.nextInt();
 		int sumResult = x + y;
---
+++ Tasks/Task2/Program.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Program {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		
+		int x0;
+		int x1;
+		int x2;
+		int x3;
+		int x4;
+		int x5;
+		int x6;
+		int x7;
+		
+		System.out.println(x7 + "" 
+			+ x6 + "" 
+			+ x5 + "" 
+			+ x4 + ""
+			+ x3 + ""
+			+ x2 + ""
+			+ x1 + ""
+			+ x0);
+
+	}
+}
