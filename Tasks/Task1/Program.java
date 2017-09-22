0a35cb616fea823e2e3ca248447132938274e65b
student21 <student21@itpark.ru>
2017-09-22 20:40:02 +0300

    fix gitignore and add tasks

---
+++ Tasks/Task1/Program.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Program {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		int sumResult = x + y;
+
+		System.out.println(sumResult);
+	}
+}
