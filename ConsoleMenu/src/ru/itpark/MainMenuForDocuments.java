b9a5988af0a0989955b444b043329b6f1ff51d75
student21 <student21@itpark.ru>
2017-10-02 20:57:17 +0300

    prepare for homework

---
+++ ConsoleMenu/src/ru/itpark/MainMenuForDocuments.java
@@ -0,0 +1,8 @@
+package ru.itpark;
+
+public class MainMenuForDocuments {
+    public static void main(String[] args) {
+        Document documents[] = new Document[10];
+        // меню должно позволять работать с отдельным документом
+    }
+}
