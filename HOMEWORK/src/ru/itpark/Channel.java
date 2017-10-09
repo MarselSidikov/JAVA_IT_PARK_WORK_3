8ad36fe43f66d03beb78d93c5d13d8fa6bc64fff
student21 <student21@itpark.ru>
2017-10-09 21:05:40 +0300

    add homework;

---
+++ HOMEWORK/src/ru/itpark/Channel.java
@@ -0,0 +1,5 @@
+package ru.itpark;
+
+public class Channel {
+    private Program programs[];
+}
---
+++ HOMEWORK/src/ru/itpark/Program.java
@@ -0,0 +1,9 @@
+package ru.itpark;
+
+import java.time.LocalTime;
+
+public class Program {
+    private String name;
+    private LocalTime beginTime;
+    private LocalTime endTime;
+}
