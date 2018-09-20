7e57095f47d4b6479af3d0b8c78fe00856eae8ce
MarselSidikov <sidikov.marsel@gmail.com>
2018-09-20 10:27:26 +0300

    fix thread pool

--- ThreadsExtended/src/ru/itpark/MainThreadPool.java
+++ ThreadsExtended/src/ru/itpark/MainThreadPool.java
@@ -1,25 +1,48 @@
 package ru.itpark;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainThreadPool {
-  public static void main(String[] args) {
-    ThreadPool threadPool = new ThreadPool(2);
+    public static void main(String[] args) {
+//    ThreadPool threadPool = new ThreadPool(2);
+//
+//    threadPool.submitTask(() -> {
+//      for (int i = 0; i < 1000; i++) {
+//        System.out.println(Thread.currentThread().getName() + " A");
+//      }
+//    });
+//
+//    threadPool.submitTask(() -> {
+//      for (int i = 0; i < 1000; i++) {
+//        System.out.println(Thread.currentThread().getName() + " B");
+//      }
+//    });
+//
+//    threadPool.submitTask(() -> {
+//      for (int i = 0; i < 1000; i++) {
+//        System.out.println(Thread.currentThread().getName() + " C");
+//      }
+//    });
 
-    threadPool.submitTask(() -> {
-      for (int i = 0; i < 1000; i++) {
-        System.out.println(Thread.currentThread().getName() + " A");
-      }
-    });
+        ExecutorService service = Executors.newFixedThreadPool(5);
 
-    threadPool.submitTask(() -> {
-      for (int i = 0; i < 1000; i++) {
-        System.out.println(Thread.currentThread().getName() + " B");
-      }
-    });
+        service.submit(() -> {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(Thread.currentThread().getName() + " A");
+            }
+        });
 
-    threadPool.submitTask(() -> {
-      for (int i = 0; i < 1000; i++) {
-        System.out.println(Thread.currentThread().getName() + " C");
-      }
-    });
-  }
+        service.submit(() -> {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(Thread.currentThread().getName() + " B");
+            }
+        });
+
+        service.submit(() -> {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(Thread.currentThread().getName() + " C");
+            }
+        });
+    }
 }
